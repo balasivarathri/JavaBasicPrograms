ea2080187dbb0fe183c7db19be0bf5ae21455f33
balasivarathri <77465585+balasivarathri@users.noreply.github.com>
2021-11-29 22:01:40 +0200

    morongwa 2nd user 1st commit

--- src/com/javabasicprograms/IfCondition.java
+++ src/com/javabasicprograms/IfCondition.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 
 public class IfCondition {
     public static void main(String[] args) {
-        System.out.print("Please Enter A Number: ");
-        Scanner sc = new Scanner(System.in);
+        /*System.out.print("Please Enter A Number: ");
+        Scanner sc = new Scanner(System.in);*/
        /* int n = sc.nextInt();
         if (n % 2 == 0)
             System.out.println("Even Number");
         if (!(n % 2 == 0))
             System.out.println("Odd Number");*/
-        if (sc.nextInt() % 2 == 0) {
+        /*if (sc.nextInt() % 2 == 0) {
             System.out.println("Even Number");
         } else {
             System.out.println("Odd Number");
+        }*/
+
+        for (int i = 0; i < 100; ++i) {
+            if (i % 2 == 0)
+                System.out.println(i+" : is a Even Number");
+            else
+                System.out.println(i+" : is a Odd Number");
         }
     }
 }
