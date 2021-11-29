44446417fcfd42204b738ab1f1c6b6c2bf4aa0bc
balasivarathri <77465585+balasivarathri@users.noreply.github.com>
2021-11-29 21:16:59 +0200

    morongwa commit

--- src/com/javabasicprograms/IfCondition.java
+++ src/com/javabasicprograms/IfCondition.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class IfCondition {
     public static void main(String[] args) {
+        System.out.println("This code is written by Bala: ");
         System.out.print("Please Enter A Number: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
