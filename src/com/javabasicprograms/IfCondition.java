b5198aa8567a98186c88d2f4e621dbfbd35bccba
balasivarathri <77465585+balasivarathri@users.noreply.github.com>
2021-11-29 21:27:17 +0200

    morongwa 2nd commit

--- src/com/javabasicprograms/IfCondition.java
+++ src/com/javabasicprograms/IfCondition.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class IfCondition {
     public static void main(String[] args) {
-        System.out.println("This code is written by Bala: ");
         System.out.print("Please Enter A Number: ");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+       /* int n = sc.nextInt();
         if (n % 2 == 0)
             System.out.println("Even Number");
         if (!(n % 2 == 0))
+            System.out.println("Odd Number");*/
+        if (sc.nextInt() % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
             System.out.println("Odd Number");
+        }
     }
 }
